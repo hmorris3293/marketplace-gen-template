8effc40e8d22f7b78f7e273fd009c2ca0cdfc3f5
hmorris3293 <morrisholdenx@gmail.com>
2024-01-23 20:52:50 -0500

    Update gitgen.sh

--- gitgen.sh
+++ gitgen.sh
@@ -7,15 +7,16 @@ generate_uuid4() {
 
 # Check if the app name argument is provided
 if [ -z "$1" ]; then
-  echo "Usage: $0 <app_name>"
+  echo "Usage: $0 <new_app_name>"
   exit 1
 fi
 
 # Variables
 repo_directory="path/to/marketplace-apps"
-app_name="$1"
-new_branch="${app_name}-$(generate_uuid4)"
+new_app_name="$1"
+new_branch="${new_app_name}-$(generate_uuid4)"
 gen_repo="https://github.com/hmorris3293/marketplace-gen-template.git"
+gen_dir="/tmp/marketplace-gen-template"
 
 # Change directory to the repository
 cd "$repo_directory" || exit
@@ -38,7 +39,11 @@ echo "Successfully checked out branch: $new_branch"
 git -C /tmp clone $gen_repo
 
 # CD into template repo
-cd /tmp/marketplace-gen-template
+cd $gen_dir
+
+# find & replace all $app_name to $new_app_name aka $1
+find $gen_dir -type f -exec sed -i 's/\$app_name/$new_app_name/g' {} + -o -type d -exec bash -c 'mv "$0" "${0/\$app_name/$new_app_name}"' {} \;
+
 
 
 
