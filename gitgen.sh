786719395cb3ec8ce2ac6fcdaa49ce2a41f357d4
hmorris3293 <morrisholdenx@gmail.com>
2024-01-23 21:08:45 -0500

    Update gitgen.sh

--- gitgen.sh
+++ gitgen.sh
@@ -39,10 +39,33 @@ echo "Successfully checked out branch: $new_branch"
 git -C /tmp clone $gen_repo
 
 # CD into template repo
-cd $gen_dir
+# cd $gen_dir don't need this
 
 # find & replace all $app_name to $new_app_name aka $1
 find $gen_dir -type f -exec sed -i 's/\$app_name/$new_app_name/g' {} + -o -type d -exec bash -c 'mv "$0" "${0/\$app_name/$new_app_name}"' {} \;
+
+# Copy all things over to new branch
+## app
+cp -R $gen_dir/apps $repo_directory/apps
+## deployment_script
+cp -R $gen_dir/apps $repo_directory/deployment_scripts
+
+# Verify
+app_directory="$repo_directory/apps/linode-marketplace-$new_app_name"
+
+if [ -d "$app_directory" ]; then
+    echo "The directory exists: $app_directory"
+    echo "All systems GO"
+else
+    echo "Error: The directory does not exist: $app_directory"
+    echo "Elvis or Pat Broke something...."
+    exit 1
+fi
+
+# Generation complete
+echo "Operation new Marketplace app, COMPLETE!"
+
+
 
 
 
